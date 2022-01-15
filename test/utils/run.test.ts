518429b4c443090a84421abbaefb696f1343d000
Hussain Abbas <howdy@hussainabbas.com>
2022-01-15 02:06:19 -0500

    test: fix run unit test for windows

--- test/utils/run.test.ts
+++ test/utils/run.test.ts
@@ -2,6 +2,8 @@ import { run } from '../../src/utils'
 
 describe('run', () => {
   it('should run a command on the command-line and return a string', async () => {
-    expect(await run('echo "hello world"')).toEqual('hello world')
+    expect((await run('echo "hello world"')).replace(/[^a-z ]/gi, '')).toEqual(
+      'hello world'
+    )
   })
 })
