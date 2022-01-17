18e4030c3bda8d4d1758cd3169a9f45e2ebb5cd0
Hussain Abbas <howdy@hussainabbas.com>
2022-01-16 21:09:54 -0500

    docs: add jsdocs for each function

--- src/index.ts
+++ src/index.ts
@@ -14,18 +14,38 @@ async function runLibCamera(
   return run(command)
 }
 
+/**
+ * Wrapper function for the [`libcamera-jpeg` command](https://www.raspberrypi.com/documentation/accessories/camera.html#libcamera-jpeg)
+ *
+ * @param options The options object sent to `libcamera-jpeg` as command-line flags
+ */
 export function jpeg(options: Partial<LibCamera.StillOptionsObject>) {
   return runLibCamera('libcamera-jpeg', options)
 }
 
+/**
+ * Wrapper function for the [`libcamera-still` command](https://www.raspberrypi.com/documentation/accessories/camera.html#libcamera-still)
+ *
+ * @param options The options object sent to `libcamera-still` as command-line flags
+ */
 export function still(options: Partial<LibCamera.StillOptionsObject>) {
   return runLibCamera('libcamera-still', options)
 }
 
+/**
+ * Wrapper function for the [`libcamera-vid` command](https://www.raspberrypi.com/documentation/accessories/camera.html#libcamera-vid)
+ *
+ * @param options The options object sent to `libcamera-vid` as command-line flags
+ */
 export function vid(options: Partial<LibCamera.VideoOptionsObject>) {
   return runLibCamera('libcamera-vid', options)
 }
 
+/**
+ * Wrapper function for the [`libcamera-raw` command](https://www.raspberrypi.com/documentation/accessories/camera.html#libcamera-raw)
+ *
+ * @param options The options object sent to `libcamera-raw` as command-line flags
+ */
 export function raw(options: Partial<LibCamera.VideoOptionsObject>) {
   return runLibCamera('libcamera-raw', options)
 }
