fa94b0560ef7d5193e38d67e630f1e35af82d7d2
Hussain Abbas <howdy@hussainabbas.com>
2022-01-16 23:09:21 -0500

    test:  add test for network streaming command

--- test/libcamera/vid.test.ts
+++ test/libcamera/vid.test.ts
@@ -12,4 +12,17 @@ describe('libcamera.vid', () => {
       'libcamera-vid --output test.h264 --timeout 10000 --save-pts timestamps.txt'
     )
   })
+
+  it('should return the command used for running a network stream', async () => {
+    expect(
+      await libcamera.vid({
+        timeout: 0,
+        inline: true,
+        listen: true,
+        output: 'tcp://0.0.0.0:1234',
+      })
+    ).toEqual(
+      'libcamera-vid --timeout 0 --inline --listen --output tcp://0.0.0.0:1234'
+    )
+  })
 })
